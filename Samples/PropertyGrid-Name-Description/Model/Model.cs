f8e2628b4f76b5c1bdeb5952cb311f1d963c0dc6
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-02-12 16:26:42 +0530

    Name and description sample committed.

---
+++ Samples/PropertyGrid-Name-Description/Model/Model.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PropertyGrid_Name_Description
+{
+    public class Employee
+    {        
+        // Display name and description for the following properties sets by using "DisplayName" and "Display attribute's Description field".
+        [DisplayName("Email ID")]
+        [Display(Description = "Mail ID of the Employee")]
+        public string Mail { get; set; }
+
+        [DisplayName("Address")]
+        [Display(Description = "Address of the Employee")]
+        public string Add { get; set; }
+
+        [DisplayName("Employee Name")]
+        [Display(Description = "Name of the Employee")]
+        public string Name { get; set; }
+
+        [DisplayName("Date of Birth")]
+        [Display(Description = "Birth date of the Employee")]
+        public DateTime DOB { get; set; }
+
+
+
+        // Display name and description for the following properties sets by using Display attribute's Name field" and "Description" attribute.
+        [Display(Name = "Gender")]
+        [Description("Gender of the Employee")]
+        public Gender Gen { get; set; }
+
+        [Display(Name = "Employee ID")]
+        [Description("ID of the Employee")]
+        public string ID { get; set; }
+
+        [Description("Age of the Employee")]
+        public int Age { get; set; }
+
+        [Display(Name = "Experience")]
+        [Description("Experience of the Employee")]
+        public int Exp { get; set; }
+    }
+    public enum Gender
+    {
+        Male, Female
+    }
+
+}
---
+++ Samples/PropertyGrid-Name-Description/ViewModel/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PropertyGrid_Name_Description
+{
+    public class ViewModel :NotificationObject
+    {
+        private Visibility descriptionPanelVisibility;
+
+        public Object SelectedEmployee { get; set; }
+        public Visibility DescriptionPanelVisibility
+        {
+            get { return descriptionPanelVisibility; }
+            set
+            {
+                descriptionPanelVisibility = value;
+                this.RaisePropertyChanged(nameof(DescriptionPanelVisibility));
+            }
+        }
+        public ViewModel()
+        {
+            SelectedEmployee = new Employee()
+            {
+                Name = "John",
+                ID = "381",
+                DOB = new DateTime(1995, 12, 24),
+                Add="New Yark",
+                Age=30,
+                Exp=3,
+                Gen= Gender.Male,
+                Mail="john@gta.com"
+            };
+        }
+    }
+}
