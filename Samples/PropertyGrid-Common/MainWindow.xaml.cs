03e3c9599189643dc0fcea2acd5cd319c51e849a
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-02-18 15:18:15 +0530

    sample changes done

---
+++ Samples/PropertyGrid-Common/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace PropertyGrid_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// <summary>
+    public partial class MainWindow : Window
+    {       
+        /// <summary> 
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.    
+        /// </summary>       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    
+}
