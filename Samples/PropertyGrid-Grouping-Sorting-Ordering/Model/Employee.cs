4c5cc892ace2ed3b8cc1bea4c0baa5b26a19c796
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-02-18 12:23:55 +0530

    Grouping, sorting and ordering sample committed.

---
+++ Samples/PropertyGrid-Grouping-Sorting-Ordering/Model/Employee.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grouping_Sorting_Ordering
+{
+    class Employee
+    {
+        /// <summary>
+        /// Gets or sets the Address of the Employee.
+        /// </summary>        
+        [Display(Order = 3)]
+        [Category("Basic Info")]
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Mobile number of the Employee.
+        /// </summary>
+        [Display(Order = 8)]
+        public string MobileNo { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Experience of the Employee.
+        /// </summary>
+        [Display(Order = 6)]
+        [Category("Employee Info")]
+        public int Experience { get; set; }
+
+        /// <summary>
+        /// Gets or sets of the Email Id of the Employee.
+        /// </summary>
+        [Display(Order = 7)]
+        public string EmailID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Name of the Employee.
+        /// </summary>
+        [Display(Order = 0)]
+        [Category("Basic Info")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Date of Birth of the Employee.
+        /// </summary>
+        [Display(Order = 2)]
+        [Category("Basic Info")]
+        public DateTime DOB { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Gender of the Employee.
+        /// </summary> 
+        [Display(Order = 4)]
+        [Category("Basic Info")]
+        public Gender Gender { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the Employee.
+        /// </summary>
+        [Category("Basic Info")]
+        public string ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Age of the Employee.
+        /// </summary>
+        [Display(Order = 3)]
+        [Category("Basic Info")]
+        public int Age { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the Employee.
+        /// </summary>
+        [Display(Order = 5)]
+        [Category("Employee Info")]
+        public EmployeeType EmployeeType { get; set; }
+    }
+    public enum EmployeeType
+    {
+        Fulltime_Employee,
+        Parttime_Employee
+    }
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
---
+++ Samples/PropertyGrid-Grouping-Sorting-Ordering/ViewModel/ViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Windows;
+
+namespace Grouping_Sorting_Ordering
+{
+    public class ViewModel : NotificationObject
+    {
+        private Visibility searchBoxVisibility;
+        private Object selectedEmployee;
+        private bool enableGrouping;
+
+        public Visibility SearchBoxVisibility
+        {
+            get { return searchBoxVisibility; }
+            set
+            {
+                searchBoxVisibility = value;
+                this.RaisePropertyChanged(nameof(SearchBoxVisibility));
+            }
+        }
+        public bool EnableGrouping
+        {
+            get { return enableGrouping; }
+            set
+            {
+                enableGrouping = value;
+                this.RaisePropertyChanged(nameof(EnableGrouping));
+            }
+        }
+
+        public object SelectedEmployee
+        {
+            get { return selectedEmployee; }
+            set
+            {
+                selectedEmployee = value;
+                this.RaisePropertyChanged(nameof(SelectedEmployee));
+            }
+        }
+
+        public ViewModel()
+        {
+            SelectedEmployee = new Employee()
+            {
+                Address = "10,Martin Street, New Yark",
+                Age = 30,
+                DOB = new DateTime(1990, 04, 19),
+                EmailID = "johnson@gta.com",
+                EmployeeType = EmployeeType.Fulltime_Employee,
+                Experience = 5,
+                Gender = Gender.Male,
+                ID = "678",
+                MobileNo = "48762345",
+                Name = "John Son"
+            };
+        }
+    }
+}
