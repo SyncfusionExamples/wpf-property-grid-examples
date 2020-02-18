95ebb3ba37cac5eaa191bcca76c2348a14184b6a
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-02-18 11:12:56 +0530

    Readonly sample committed.

---
+++ Samples/PropertyGrid-ReadOnlyProperty/Model/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyGrid_ReadOnlyProperty
+{
+    class Employee {
+        [ReadOnly(true)]
+        public string EmailID { get; set; }
+        [Editable(false)]
+        public string Name { get; set; }
+        [Editable(false)]
+        public Gender Gender { get; set; }
+        [ReadOnly(true)]
+        public string ID { get; set; }
+        //ReadOnly attribute have higer priority than the Editable attribute.
+        [ReadOnly(true)]
+        [Editable(true)]
+        public DateTime DOB { get; set; }
+    }
+    public enum Gender {
+        Male, 
+        Female
+    }
+}
---
+++ Samples/PropertyGrid-ReadOnlyProperty/ViewModel/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyGrid_ReadOnlyProperty
+{
+    public class ViewModel
+    {
+        public Object SelectedEmployee { get; set; }
+        public ViewModel()
+        {
+            SelectedEmployee = new Employee()
+            {
+                Name = "John Son",
+                DOB = new DateTime(1990, 01, 28),
+                EmailID = "johnson@gta.com",
+                Gender = Gender.Male,
+                ID = "397"                
+            };
+        }
+    }
+}
