b4f37c630ee02bee0fc86b0959dfe3ed9c1e8128
subashini.mahendran <subashini.mahendran@syncfusion.com>
2020-04-29 19:41:09 +0530

    Added demo regarding Key down handling support in PropertyGrid

---
+++ Samples/CustomEditor/How-to-prevent-moving-focus-to-next-editor-propertygrid/CustomEditor.cs
@@ -0,0 +1,74 @@
+﻿using Syncfusion.Windows.PropertyGrid;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace PropertyGrid_FocusHandling
+{
+    public class ComboBoxEditor : BaseTypeEditor
+    {
+        ComboBox enumCombo;
+
+        public override void Attach(PropertyViewItem property, PropertyItem info)
+        {
+            var binding = base.CreatePropertyInfoBinding(info, enumCombo);
+            BindingOperations.SetBinding(enumCombo, ComboBox.SelectedItemProperty, binding);
+        }
+
+        public override object Create(PropertyInfo PropertyInfo)
+        {
+            return this.CreateEditor(PropertyInfo.PropertyType);
+        }
+
+        public override object Create(PropertyDescriptor PropertyDescriptor)
+        {
+            return this.CreateEditor(PropertyDescriptor.PropertyType);
+        }
+
+        public override void Detach(PropertyViewItem property)
+        {
+            if (enumCombo != null)
+            {
+                BindingOperations.ClearAllBindings(enumCombo);
+                BindingOperations.ClearBinding(enumCombo, ComboBox.SelectedItemProperty);
+            }
+            enumCombo.ItemsSource = null;
+            enumCombo.Items.Clear();
+            enumCombo = null;
+        }
+
+        public override bool ShouldPropertyGridTryToHandleKeyDown(Key key)
+        {
+            if (key == Key.Up || key == Key.Down)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Creates and initializes a new instance of the ComboBox editor.
+        /// </summary>
+        /// <param name="propertyType">The property type</param>
+        /// <returns>The EnumComboEditor</returns>
+        private ComboBox CreateEditor(Type propertyType)
+        {
+            enumCombo = new ComboBox()
+            {
+                ItemsSource = EnumHelper.GetValues(propertyType),
+                BorderThickness = new Thickness(0)
+            };
+            return enumCombo;
+        }
+
+    }
+}
---
+++ Samples/CustomEditor/How-to-prevent-moving-focus-to-next-editor-propertygrid/Person.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyGrid_FocusHandling
+{
+    [Editor("Gender", typeof(ComboBoxEditor))]
+    public class Person
+    {
+        public Person()
+        {
+            FirstName = "Carl";
+            LastName = "Johnson";
+            Age = 30;
+            Mobile = 91983467382;
+            Email = "carljohnson@gta.com";
+            ID = "0005A";
+            DOB = new DateTime(1987, 10, 16);
+            Gender = Gender.Male;
+        }
+
+        public Gender Gender
+        {
+            get;
+
+            set;
+        }
+
+        public string Email
+        {
+            get;
+
+            set;
+        }
+
+        public string FirstName
+        {
+            get;
+
+            set;
+        }
+        
+        public string LastName
+        {
+            get;
+
+            set;
+        }
+
+        public string ID
+        {
+            get;
+
+            set;
+        }
+
+        public DateTime DOB
+        {
+            get;
+
+            set;
+        }
+
+
+        public long Mobile
+        {
+            get;
+
+            set;
+        }
+
+        public int Age
+        {
+            get;
+
+            set;
+        }
+    }
+
+
+    public enum Gender
+    {
+        Male,
+
+        Female
+    }
+}
